952a48ab5e88a334a28659404d4047ab3084b476
Chidera Nwachukwu <chideranwachukwu32@gmail.com>
2025-08-27 22:47:19 +0100

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
                                                                                                            <img 
                 src="/Blue Icon.png" 
                 alt="SeekCa" 
-                className="h-12 w-auto"
+                className="h-10 w-auto"
               />
             </div>
             <p className="text-gray-400 max-w-md">
